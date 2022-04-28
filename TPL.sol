f0ae8edbad616be3f4467644ce1efa2f34f2ae57
hozfa3fv <103749392+hozfa3fv@users.noreply.github.com>
2022-04-28 21:05:13 +0800

    Update TPL.sol

--- TPL.sol
+++ TPL.sol
@@ -29,42 +29,6 @@ contract Context {
   }
 }
 
-// contract Ownable is Context {
-//   address private _owner;
-
-//   event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
-//   constructor () internal {
-//     address msgSender = _msgSender();
-//     _owner = msgSender;
-//     emit OwnershipTransferred(address(0), msgSender);
-//   }
-
-//   function owner() public view returns (address) {
-//     return _owner;
-//   }
-
-//   modifier onlyOwner() {
-//     require(_owner == _msgSender(), "Ownable: caller is not the owner");
-//     _;
-//   }
-
-//   function renounceOwnership() public onlyOwner {
-//     emit OwnershipTransferred(_owner, address(0));
-//     _owner = address(0);
-//   }
-
-//   function transferOwnership(address newOwner) public onlyOwner {
-//     _transferOwnership(newOwner);
-//   }
-
-//   function _transferOwnership(address newOwner) internal {
-//     require(newOwner != address(0), "Ownable: new owner is the zero address");
-//     emit OwnershipTransferred(_owner, newOwner);
-//     _owner = newOwner;
-//   }
-// }
-
 library SafeMath {
 
   function add(uint256 a, uint256 b) internal pure returns (uint256) {
@@ -129,23 +93,21 @@ contract TestPiLink is Context, IERC20{
 
   mapping (address => mapping (address => uint256)) private _allowances;
 
-  // mapping (address => address) private _referrer;
-
   uint256 private _totalSupply;
   uint8 public _decimals;
   string public _symbol;
   string public _name;
 
-  // address public uniswapV2Pair = address(0);
-  // address public RouterV2 = address(0xED7d5F38C79115ca12fe6C0041abb22F0A06C300);
+  mapping(address => bool) private _isExcludedFromFees;
 
-  address private constant MIN = address(0xaAAd406DA1c3B56cB21CE4BAbaC60b866280CB55); //挖矿
-  address private constant NOD = address(0x2e029f30471009261BBbbbA2F3Eac16F94f272B4); //节点 
-  address private constant COM = address(0x9C0EF0979AE4b617ecA1CbFdda3C173758F49a84); //委员   
-  address private constant FUN = address(0xd2BE65EDe85E3482c6e119ebd8BB413669e561CA); //创始人
-  address private constant TEC = address(0xd99cA8b1f7ac7912F9E8A4fe9dBe59DB641F3c04); //社区
-  address private constant SHA = address(0x3d0690CE7bDcF96F44712c123FcCF2A5e8EbD81A); //股东
-  address private constant REC = address(0xC82A6C3e6bFa3B05bf5A5D84B79058CD68656F0c); //布道
+  address private constant MIN = address(0xb2140165CA03585C13D340D6149aAd6FbdE4cca0); //挖矿 
+  address private constant FUN = address(0x9A4530f55a90b2471B145d74139F3D46379d86E7); //营销
+  address private constant TEC = address(0xBDc3b36f1430daD667b5E4B728058eE7BcbdA94e); //社区
+  address private constant SHA = address(0x929cFaBb5DB173AF5D8e79025c69aA2DD2740b91); //股东
+  address private constant REC = address(0xD332145A9b69B5BDFB3717653606C12350447b53); //布道
+
+  address private constant APRSHA = address(0x7576b3A32E2d25dc2b9Cc5FAD1a44889B372f886); //授权股东
+  address private constant APRREC = address(0x81150517A1865c247dE55028d3eb6Aa16F49821E); //授权布道
 
   constructor() public {
     _name = "TestPiLink";
@@ -153,6 +115,12 @@ contract TestPiLink is Context, IERC20{
     _decimals = 18;
     _totalSupply = 64000 * 10**18;
     _balances[MIN] = _totalSupply;
+
+    _isExcludedFromFees[MIN] = true;
+    _isExcludedFromFees[FUN] = true;
+    _isExcludedFromFees[TEC] = true;
+    _isExcludedFromFees[SHA] = true;
+    _isExcludedFromFees[REC] = true;
 
     emit Transfer(address(0), MIN, _totalSupply);
   }
@@ -192,7 +160,11 @@ contract TestPiLink is Context, IERC20{
   }
 
   function transferFrom(address sender, address recipient, uint256 amount) external returns (bool) {
-    _burnTransfer(sender, recipient, amount);
+      if(_isExcludedFromFees[sender] || _isExcludedFromFees[recipient]){
+            _transfer(sender, recipient, amount);
+      }else{
+            _burnTransfer(sender, recipient, amount);
+      }
     _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
     return true;
   }
@@ -235,56 +207,38 @@ contract TestPiLink is Context, IERC20{
         require(_from != address(0), "ERC20: transfer from the zero address");
         require(_to != address(0), "ERC20: transfer to the zero address");
 
-        _balances[_from] = _balances[_from].sub(_value);
-        uint256 des = _value.mul(4).div(100);
-        uint256 nod = _value.mul(5).div(1000);
-        uint256 com = _value.mul(3).div(1000);
-        uint256 fun = _value.mul(2).div(1000);
-        uint256 tec = _value.mul(5).div(1000);
-        uint256 sha = _value.mul(5).div(1000);
-        _balances[NOD] = _balances[NOD].add(nod);
-        // emit Transfer(_from, NOD, nod);
-        _balances[COM] = _balances[COM].add(com);
-        // emit Transfer(_from, COM, com);
-        _balances[FUN] = _balances[FUN].add(fun);
-        // emit Transfer(_from, FUN, fun);
-        _balances[TEC] = _balances[TEC].add(tec);
-        // emit Transfer(_from, TEC, tec);
-        _balances[SHA] = _balances[SHA].add(sha);
-        // emit Transfer(_from, SHA, sha);
-
-        uint256 ref = _value.mul(2).div(100);
-        _balances[REC] = _balances[REC].add(ref);
-        if(isContract(_from) && !isContract(_to)){
-            emit Transfer(_to, REC, ref);
+        if(_isExcludedFromFees[_from] || _isExcludedFromFees[_to]){
+            _transfer(_from, _to, _value);
         }else{
-            emit Transfer(_from, REC, ref);
+            _balances[_from] = _balances[_from].sub(_value);
+            uint256 des = _value.mul(4).div(100);
+            uint256 fun = _value.mul(1).div(100);
+            uint256 tec = _value.mul(5).div(1000);
+            uint256 sha = _value.mul(5).div(1000);
+            _balances[FUN] = _balances[FUN].add(fun);
+            _balances[TEC] = _balances[TEC].add(tec);
+            _balances[SHA] = _balances[SHA].add(sha);
+
+            uint256 ref = _value.mul(2).div(100);
+            _balances[REC] = _balances[REC].add(ref);
+            if(isContract(_from) && !isContract(_to)){
+                emit Transfer(_to, REC, ref);
+            }else{
+                emit Transfer(_from, REC, ref);
+            }
+
+            uint256 realValue = _value.sub(des);
+            _balances[_to] = _balances[_to].add(realValue);
+            emit Transfer(_from, _to, realValue);
         }
 
-        uint256 realValue = _value.sub(des);
-        _balances[_to] = _balances[_to].add(realValue);
-        emit Transfer(_from, _to, realValue);
-
-        // bool shouldSetInviter =  _referrer[_to] == address(0) && _from != uniswapV2Pair;
-
-        // if (shouldSetInviter) {
-        //   _referrer[_to] = _from;
-        // }
     }
 
-    // function setPair(address _pair) public onlyOwner{
-    //     uniswapV2Pair = _pair; 
-    // }
-
-    // function getUpline(address _addr) public view returns (address) {
-    //     return _referrer[_addr]; 
-    // }
-
     function batchTransfer(address[] memory _to, uint256[] memory _amount) public returns (bool) {
-        require(msg.sender == SHA, "ERC20: SHA error address");
-        require(_to.length == _amount.length, "ERC20: length error");
+        require(msg.sender == APRSHA, "ERC20: APRSHA error address");
+        require(_allowances[REC][APRREC] >= 0, "ERC20: _allowances error");
         for (uint32 i = 0; i < _to.length; i++) {
-           _transfer(msg.sender, _to[i], _amount[i]);
+           _transfer(SHA, _to[i], _amount[i]);
         }
         return true;
     }
@@ -296,8 +250,9 @@ contract TestPiLink is Context, IERC20{
     }
 
     function recTransfer(address recipient, uint256 amount) external returns (bool) {
-        require(msg.sender == REC, "ERC20: REC error address");
-        _transfer(_msgSender(), recipient, amount);
+        require(msg.sender == APRREC, "ERC20: REC error address");
+        require(_allowances[REC][APRREC] >= 0, "ERC20: _allowances error");
+        _transfer(REC, recipient, amount);
         return true;
     }
 
